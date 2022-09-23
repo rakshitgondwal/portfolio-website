23324b55c7570a09b5ef1715debbb5fca0dabadc
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-09-24 00:27:13 +0530

    ..

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,9 +7,6 @@ const Navbar = () => {
 
         <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-black">
             <div class="container flex flex-wrap justify-between items-center mx-auto">
-                {/* <a class="flex items-center">
-                    <span className=" ml-2 text-4xl whitespace-nowrap dark:text-white font-signature">Rakshit</span>
-                </a> */}
                 <Signature />
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
