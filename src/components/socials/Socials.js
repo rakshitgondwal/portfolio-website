38f32820de436dfe7fe5590553dd371feabcc01d
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-10-18 23:03:20 +0530

    setup linkedin

--- src/components/socials/Socials.js
+++ src/components/socials/Socials.js
@@ -1,17 +1,20 @@
-import React from 'react' 
-import { FaLinkedinIn } from 'react-icons/fa'  
+import React from 'react'
+import { FaLinkedinIn } from 'react-icons/fa'
 
 const Socials = () => {
   return (
     <div className='flex flex-col top-[35%] left-0 fixed'>
-        <ul>
-            <li className=' flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:rounded-md duration-300 bg-gray-600'>
-                <a href='' className='flex justify-between items-center w-full text-white'/>
-                <>
-                LinkedIn <FaLinkedinIn size={30}/>
-                </>
-            </li>
-        </ul>
+      <ul>
+      <div className='bg-cyan-900 hover:hue-rotate-180 duration-1000 hover:rounded-md '>
+        <li className=' flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px]  duration-300 '>  
+          <a href='https://www.linkedin.com/in/rakshit-gondwal-911223230/' className='flex justify-between items-center w-full text-white'>
+            <>
+              LinkedIn <FaLinkedinIn size={30} />
+            </>
+          </a>                
+        </li>
+        </div>
+      </ul>
     </div>
   )
 }
