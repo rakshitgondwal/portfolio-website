1acd47946a9788b44b01e1690632b8e90e4e1d5c
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-10-19 21:05:22 +0530

    socials working perfectly

--- src/components/socials/Socials.js
+++ src/components/socials/Socials.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FaLinkedinIn } from 'react-icons/fa'
-import {FiTwitter} from 'react-icons/fi'
-import {FiGithub} from 'react-icons/fi'
-import {AiOutlineMail} from 'react-icons/ai'
-import {HiOutlineDocumentText} from 'react-icons/hi'
+import { FiTwitter } from 'react-icons/fi'
+import { FiGithub } from 'react-icons/fi'
+import { AiOutlineMail } from 'react-icons/ai'
+import { HiOutlineDocumentText } from 'react-icons/hi'
 
 const Socials = () => {
 
-  const socials = [
+  const social = [
     {
       key: 1,
       link: 'https://www.linkedin.com/in/rakshit-gondwal-911223230/',
@@ -15,7 +15,8 @@ const Socials = () => {
         <>
           LinkedIn <FaLinkedinIn size={30} />
         </>
-      )
+      ),
+      download: false
     },
     {
       key: 2,
@@ -24,7 +25,8 @@ const Socials = () => {
         <>
           Twitter <FiTwitter size={30} />
         </>
-      )
+      ),
+      download: false
     },
     {
       key: 3,
@@ -33,39 +35,43 @@ const Socials = () => {
         <>
           Github <FiGithub size={30} />
         </>
-      )
+      ),
+      download: false
     },
     {
       key: 4,
-      name: 'Email',
+      link: 'mailto:rakshitgondwal3@gmail.com',
       child: (
         <>
-          LinkedIn <AiOutlineMail size={30} />
+          Email <AiOutlineMail size={30} />
         </>
-      )
+      ),
+      download: false
     },
     {
       key: 5,
       link: '../../assets/Resume.pdf',
-      download: true,
       child: (
         <>
           Resume <HiOutlineDocumentText size={30} />
         </>
-      )
+      ),
+      download: true
     }
   ]
 
 
   return (
-    <div className='flex flex-col top-[35%] left-0 fixed'>
+    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
       <ul>
-        {socials.map((key, name, icon, link, download,child) => (
-            <li key={key} className=' flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] bg-cyan-900 hover:hue-rotate-180 duration-1000 hover:rounded-md  '>
-              <a href={link} className='flex justify-between items-center w-full text-white' download={download} target='_blank'>
-                {child}
-              </a>
-            </li>
+        {social.map(({key, name, link, download, child}) => (
+          <li key={key} className=' flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] bg-cyan-900 hover:hue-rotate-180 duration-1000 hover:rounded-md  '>
+            <a href={link} className='flex justify-between items-center w-full text-white' download={download} target='_blank'>
+              <>
+              {child} 
+              </>
+            </a>
+          </li>
         ))}
       </ul>
     </div>
