9e1b5a2c0f17f142fcacd24bb6953c2de96314b2
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-10-23 00:47:33 +0530

    contact done

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -13,12 +13,13 @@ const Contact = () => {
                             Submit the form below to get in touch with me-
                         </p>
                     </div>
-                    <div className='flex items-center justify-center'>
+                    <div className='flex flex-col items-center justify-center'>
                         <form action="/action_page.php">
                             <input type="text" id="fname" name="fname" className='bg-transparent border-2  border-color-white p-2' size={60} placeholder="Enter you name" /><br /><br />
                             <input type="text" id="lname" name="email" className='bg-transparent border-2 border-color-white p-2' size={60} placeholder="Enter you email" /><br /><br />
-                            <textarea  name="comment" rows={4} cols={60} className='bg-transparent border-2  border-color-white p-2'
-                            placeholder='Enter your message' />
+                            <textarea  name="comment" rows={4} cols={60} className='bg-transparent border-2  border-color-white p-2'></textarea><br />
+                            <button type='submit' className='group text-white w-fit px-3 py-2 my-4 flex justify-center items-center rounded-md bg-gradient-to-r
+             from-cyan-500 to-blue-500 hover:hue-rotate-180 duration-500 cursor-pointer '>Submit</button>
                         </form>
                     </div>
                 </div>
