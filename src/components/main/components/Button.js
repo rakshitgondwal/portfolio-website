1ed8836dddca5b79a42bd73ba5b50c71ad2fc685
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-10-31 15:13:59 +0530

    added smooth scroll for main

--- src/components/main/components/Button.js
+++ src/components/main/components/Button.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BsArrowRightCircle } from 'react-icons/bs'
+import { Link } from "react-scroll";
+
 
 const Button = () => {
     return (
         <button className='group text-white w-fit px-5 py-3 my-2 flex justify-center items-center rounded-md bg-gradient-to-r
              from-cyan-500 to-blue-500 hover:hue-rotate-180 duration-500 cursor-pointer '>
-            Portfolio
+            <Link to="Portfolio" smooth duration={500} >Portfolio</Link>
             <span className='ml-2 group-hover:rotate-90 duration-300' >
                 <BsArrowRightCircle />
             </span>
